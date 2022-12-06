f3fc201f2337ce9650abbe2d2df645b3e441bb6a
Pat Escalona <pat.escalona7@gmail.com>
2022-12-06 05:06:57 -0600

    Tree created

--- bst.js
+++ bst.js
@@ -1,8 +1,93 @@
 // bst.js
 
+const arrayOfData = [10, 7, 4, 23, 8, 9,
+    4, 3, 5, 7, 9, 67, 6345, 324]
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+}
+
+class Tree { //tree is an array that will contain node objects
+    constructor(arrayOfData) {
+        this.root = arrayOfData[0];
+        console.log('this.root = ' + this.root);
+    }
+}
+
+const buildTree = (arrayOfData) => {
+    console.log('in buildTree');
+    const mySeachTree = new Tree(arrayOfData);
+    console.log(mySeachTree);
+    for (let i = 1; i < arrayOfData.length; i++) {
+        console.log(arrayOfData[i]);
+        let newNode = new Node(arrayOfData[i]);
+        console.log(newNode);
 
 
+    }
+    return mySeachTree;
 
-window.addEventListener("DOMContentLoaded", (e) {
+    //sort
+    //remove duplicates
+}
+
+const insertValue = (value) => {
+    console.log('in insert function');
+}
+
+const deleteValue = (value) => {
+    console.log('in delete function');
+}
+
+const find = (value) => {
+    console.log('in find function');
+}
+
+const levelOrder = (functionParameter) => {
+    console.log('in levelOrder');
+}
+
+const inorder = (functionParameter) => {
+    console.log('in inorder');
+}
+
+const preorder = (functionParameter) => {
+    console.log('in preorder');
+}
+
+const postorder = (functionParameter) => {
+    console.log('in postorder');
+}
+
+const height = (functionParameter) => {
+    console.log('in height');
+}
+
+const depth = (functionParameter) => {
+    console.log('in depth');
+}
+
+const isBalanced = (functionParameter) => {
+    console.log('in isBalanced');
+}
+
+const rebalance = (functionParameter) => {
+    console.log('in rebalance');
+}
+
+const ready = () => {
     console.log('DOM loaded and parsed');
-})
+    const returnedTree = buildTree(arrayOfData);
+    console.log('returnedTree is')
+    console.dir(returnedTree);
+}
+
+window.addEventListener("DOMContentLoaded", ready);
+
+// onDOMContentLoaded = (event) => {
+    
+// }
