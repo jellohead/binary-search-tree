f1cdf3bd81bdf4f25d90d456ef8eb88e6ff1f32b
pat <pat.escalona7@gmail.com>
2022-12-12 06:30:05 -0600

    merge to switch to desktop

--- bst.js
+++ bst.js
@@ -3,7 +3,6 @@
 const arrayOfData = [10, 7, 4, 23, 8, 9,
     4, 3, 5, 7, 9, 67, 6345, 324]
 
-const value = 10
 
 class Node {
     constructor(value) {
@@ -17,15 +16,16 @@ class Tree { //tree is an array that will contain node objects
     constructor(value) {
         this.root = new Node(value)
         // this.root = null
-        this.counter = 0
-        console.log('this.root = ')
-        console.log(this.root);
+        // this.counter = 0
+        // console.log('this.counter = ' + this.counter)
+        // console.log('this.root = ')
+        // console.log(this.root);
     }
 
     insertValue = value => {
         console.log('in insertValue w/ element = ' + value);
-        this.counter++
-        console.log('this.counter = ' + this.counter)
+        // this.counter++
+        // console.log('this.counter = ' + this.counter)
         let newNode = new Node(value)
         console.log('newNode =')
         console.log(newNode)
@@ -56,49 +56,17 @@ class Tree { //tree is an array that will contain node objects
             //compare value to root node value
         };
     }
-    deleteValue = (value) => {
-        console.log('in delete function');
-    }
-
-    find = (value) => {
-        console.log('in find function');
-    }
-
-    levelOrder = (functionParameter) => {
-        console.log('in levelOrder');
-    }
-
-    inorder = (functionParameter) => {
-        console.log('in inorder');
-    }
-
-    preorder = (functionParameter) => {
-        console.log('in preorder');
-    }
-
-    postorder = (functionParameter) => {
-        console.log('in postorder');
-    }
-
-    height = (functionParameter) => {
-        console.log('in height');
-    }
-
-    depth = (functionParameter) => {
-        console.log('in depth');
-    }
-
-    isBalanced = (functionParameter) => {
-        console.log('in isBalanced');
-    }
 }
 
 const buildTree = value => {
     // console.log(arrayOfData)
     console.log('in buildTree');
     let i = 0;
-    const mySearchTree = new Tree(arrayOfData[i]);
-    //create new node for each array item
+    // console.log(arrayOfData[i])
+    value = arrayOfData[i]
+    const mySearchTree = new Tree(value);
+    console.log('new search tree mySearchTree is')
+    console.log(mySearchTree)
     for (i = 1; i < arrayOfData.length; i++) {
         console.log('array element = ' + arrayOfData[i]);
         console.log(mySearchTree)
@@ -112,6 +80,7 @@ const buildTree = value => {
 
 const ready = () => {
     console.log('DOM loaded and parsed');
+    let value = arrayOfData[0]
     const returnedTree = buildTree(value);
     console.log('returnedTree is')
     console.dir(returnedTree);
@@ -119,3 +88,39 @@ const ready = () => {
 
 window.addEventListener("DOMContentLoaded", ready);
 
+
+// deleteValue = (value) => {
+//     console.log('in delete function');
+// }
+
+// find = (value) => {
+//     console.log('in find function');
+// }
+
+// levelOrder = (functionParameter) => {
+//     console.log('in levelOrder');
+// }
+
+// inorder = (functionParameter) => {
+//     console.log('in inorder');
+// }
+
+// preorder = (functionParameter) => {
+//     console.log('in preorder');
+// }
+
+// postorder = (functionParameter) => {
+//     console.log('in postorder');
+// }
+
+// height = (functionParameter) => {
+//     console.log('in height');
+// }
+
+// depth = (functionParameter) => {
+//     console.log('in depth');
+// }
+
+// isBalanced = (functionParameter) => {
+//     console.log('in isBalanced');
+// }
